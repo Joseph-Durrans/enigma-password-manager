9c0067a712da0faa07394a9403c0b2e384a0474e
JoeDurrans <joe@framereality.co.uk>
2024-01-14 20:27:23 +0000

    C++ code improvements

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,10 +6,6 @@
 
 class Rotor {
 public:
-    int position;
-    int increment;
-    const std::array<char, 26> wiring;
-
     Rotor(std::array<char, 26> wiring, char notch, int position = 0, int increment = 1) : wiring(wiring), notch(notch), position(position), increment(increment) {}
 
     int forward(int pos);
@@ -17,14 +13,34 @@ public:
     void incrementRotor();
     bool reachedNotch();
 
-private: 
+    int getPosition() const {
+        return this->position;
+    }
+
+    void setPosition(int position) {
+        this->position = position;
+    }
+
+    int getIncrement() const {
+        return this->increment;
+    }
+
+    void setIncrement(int increment) {
+        this->increment = increment;
+    }
+
+    std::array<char, 26> getWiring() const {
+		return this->wiring;
+	}
+private:
+    const std::array<char, 26> wiring;
+    int position;
+    int increment;
     int notch;
 };
 
 class RotorSet {
 public:
-    int repeat;
-    std::vector<Rotor*> rotors;
 
     RotorSet(std::vector<Rotor*> rotors, std::array<char, 26> reflector, int repeat = 1) : rotors(rotors), reflector(reflector), repeat(repeat) {}
 
@@ -34,8 +50,27 @@ public:
     int reflect(int pos);
     std::string encode(std::string msg);
 
+
+    std::vector<Rotor*> getRotors() const {
+		return this->rotors;
+	}
+
+    std::array<char, 26> getReflector() const {
+		return this->reflector;
+	}
+
+    int getRepeat() const {
+        return this->repeat;
+    }
+
+    void setRepeat(int repeat) {
+        this->repeat = repeat;
+    }
+
 private:
-	const std::array<char, 26> reflector;
+    std::vector<Rotor*> rotors;
+    const std::array<char, 26> reflector;
+    int repeat;
 
     static int posMod(int a, int b) {
         return (a % b + b) % b;
@@ -47,10 +82,10 @@ const std::array<char, 26> alpha = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
 int Rotor::forward(int pos)
 {
     // Get the position of the character in the alphabet:
-    for(int i = 0; i < alpha.size(); i++) {
+    for (int i = 0; i < alpha.size(); i++) {
         if (alpha[i] == this->wiring[pos]) {
-			return i;
-		}
+            return i;
+        }
     }
 
     // If the character is not found, return 0:
@@ -83,47 +118,47 @@ bool Rotor::reachedNotch() {
 void RotorSet::rotate() {
     // Get length of rotors:
     int rsLen = this->rotors.size() - 1;
- 
+
     // Rotate the rightmost rotor:
-	this->rotors[rsLen]->incrementRotor();
+    this->rotors[rsLen]->incrementRotor();
 
     // Check if double rotation is needed on any rotor:
     for (int i = rsLen - 1; i > 0; i--) {
         if (this->rotors[i]->reachedNotch()) {
-			this->rotors[i]->incrementRotor();
-		}
-	}
+            this->rotors[i]->incrementRotor();
+        }
+    }
 
     // Rotate the next rotors if this rotor has reached its notch:
     for (int i = rsLen; i > 0; i--) {
         if (this->rotors[i]->reachedNotch()) {
-			this->rotors[i-1]->incrementRotor();
-		}
-	}
+            this->rotors[i - 1]->incrementRotor();
+        }
+    }
 }
 
 int RotorSet::traverseForward(int pos) {
     for (int i = this->rotors.size() - 1; i >= 0; i--) {
-		pos = this->rotors[i]->forward(pos);
+        pos = this->rotors[i]->forward(pos);
 
         // Adjust for next rotor position:
         if (i != 0) {
-            pos = posMod(pos - this->rotors[i]->position + this->rotors[i - 1]->position, this->rotors[i]->wiring.size());
+            pos = posMod(pos - this->rotors[i]->getPosition() + this->rotors[i - 1]->getPosition(), this->rotors[i]->getWiring().size());
         }
-	}
+    }
 
     return pos;
 }
 
 int RotorSet::traverseBackward(int pos) {
     for (int i = 0; i < this->rotors.size(); i++) {
-		pos = this->rotors[i]->backward(pos);
+        pos = this->rotors[i]->backward(pos);
 
-		// Adjust for next rotor position:
+        // Adjust for next rotor position:
         if (i != this->rotors.size() - 1) {
-			pos = posMod(pos - this->rotors[i]->position + this->rotors[i + 1]->position, this->rotors[i]->wiring.size());
-		}
-	}
+            pos = posMod(pos - this->rotors[i]->getPosition() + this->rotors[i + 1]->getPosition(), this->rotors[i]->getWiring().size());
+        }
+    }
 
     return pos;
 
@@ -133,8 +168,8 @@ int RotorSet::reflect(int pos) {
     // Get the position of the character in the alphabet:
     for (int i = 0; i < alpha.size(); i++) {
         if (alpha[i] == this->reflector[pos]) {
-			return i;
-		}
+            return i;
+        }
     }
 
     // If the character is not found, return 0:
@@ -146,47 +181,47 @@ std::string RotorSet::encode(std::string msg) {
 
     for (int i = 0; i < msg.size(); i++) {
 
-        
-		// Rotate the rotors:
-		this->rotate();
-    
-		// If the character is a space, add a space to the encoded message and continue:
-        if (msg[i] == ' ') {
-			encodedMsg += ' ';
-			continue;
-		}
 
-		// Get the position of the character in the alphabet adjusted for the next rotor position:
-		int pos;
+        // Rotate the rotors:
+        this->rotate();
+
+        // If the character is a space, add a space to the encoded message and continue:
+        if (msg[i] == ' ') {
+            encodedMsg += ' ';
+            continue;
+        }
+
+        // Get the position of the character in the alphabet adjusted for the next rotor position:
+        int pos;
         for (int j = 0; j < alpha.size(); j++) {
             if (alpha[j] == msg[i]) {
-				pos = posMod(j + this->rotors[this->rotors.size() - 1]->position, alpha.size());
-			}
-		}
-        
-		// Go forward through rotors adjusting for position:
-        for (int j = 0; j < this->repeat; j++) {
-			pos = this->traverseForward(pos);
-		}
-        
-		// Reflect the signal:
-		pos = this->reflect(pos);
-        
-		// Go backward through rotors:
-        for (int j = 0; j < this->repeat; j++) {
-			pos = this->traverseBackward(pos);
-		}
-        
-		// Get the position of the character in the alphabet:
-		pos = posMod(pos - this->rotors[this->rotors.size() - 1]->position, alpha.size());
+                pos = posMod(j + this->rotors[this->rotors.size() - 1]->getPosition(), alpha.size());
+            }
+        }
 
-		// Get the character from the alphabet:
-		encodedMsg += alpha[pos];
-        
-        
-	}
+        // Go forward through rotors adjusting for position:
+        for (int j = 0; j < this->repeat; j++) {
+            pos = this->traverseForward(pos);
+        }
 
-	return encodedMsg;
+        // Reflect the signal:
+        pos = this->reflect(pos);
+
+        // Go backward through rotors:
+        for (int j = 0; j < this->repeat; j++) {
+            pos = this->traverseBackward(pos);
+        }
+
+        // Get the position of the character in the alphabet:
+        pos = posMod(pos - this->rotors[this->rotors.size() - 1]->getPosition(), alpha.size());
+
+        // Get the character from the alphabet:
+        encodedMsg += alpha[pos];
+
+
+    }
+
+    return encodedMsg;
 }
 
 
@@ -195,47 +230,47 @@ int main() {
     // Create rotors:
     RotorSet rs(
         {
-			new Rotor({ 'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J' }, 'Q', 0, 1),
-			new Rotor({ 'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E' }, 'E', 0, 1),
-			new Rotor({ 'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O' }, 'V', 0, 1)
-		},
-		{ 'Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T' },
-		1
-	);
+            new Rotor({ 'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J' }, 'Q', 0, 1),
+            new Rotor({ 'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E' }, 'E', 0, 1),
+            new Rotor({ 'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O' }, 'V', 0, 1)
+        },
+        { 'Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T' },
+        1
+    );
 
     // For each rotor in rotor set, set position and increment with user input:
-    for (Rotor* r : rs.rotors) {
+    for (Rotor* r : rs.getRotors()) {
         int rPos, rInc;
 
         try {
             std::cout << "Enter rotor position and increment (0 - 25): ";
             std::cin >> rPos >> rInc;
-            r->position = rPos;
-            r->increment = rInc;
-        } 
+        }
         catch (std::exception e) {
-			std::cout << "Error: " << e.what() << " rotor position and increment must be between 0 and 25";
+            std::cout << "Error: " << e.what() << " rotor position and increment must be between 0 and 25";
             return 0;
         }
 
-        if (r->position > 25 || r->position < 0) {
+        if (rPos > 25 || rPos < 0) {
             std::cout << "Error: rotor position must be between 0 and 25";
             return 0;
         }
 
-        if (r->increment > 25 || r->increment < 0) {
-			std::cout << "Error: rotor increment must be between 0 and 25";
+        if (rInc > 25 || rInc < 0) {
+            std::cout << "Error: rotor increment must be between 0 and 25";
             return 0;
         }
 
-        r->position = rPos;
-        r->increment = rInc;
+        r->setPosition(rPos);
+        r->setIncrement(rInc);
     }
 
     // Set repeat with user input:
     try {
-		std::cout << "Enter repeat (1 - 25): ";
-		std::cin >> rs.repeat;
+        std::cout << "Enter repeat (1 - 25): ";
+        int repeat;
+        std::cin >> repeat;
+        rs.setRepeat(repeat);
     }
     catch (std::exception e) {
         std::cout << "Error: repeat must be an integer";
@@ -243,7 +278,7 @@ int main() {
     }
 
     // Get message from user:
-	std::string msg;
+    std::string msg;
     std::cout << "Enter message: ";
     std::cin.ignore();
     std::getline(std::cin, msg);
@@ -255,10 +290,10 @@ int main() {
     for (int i = 0; i < msg.size(); i++) {
         auto a = std::find(std::begin(alpha), std::end(alpha), msg[i]);
         if (a == std::end(alpha)) {
-			std::cout << "Error: message contains invalid characters";
-			return 0;
-		}
-	}
+            std::cout << "Error: message contains invalid characters";
+            return 0;
+        }
+    }
 
     // Print the encoded message:
     std::cout << "Encoded message: " << rs.encode(msg) << std::endl;
